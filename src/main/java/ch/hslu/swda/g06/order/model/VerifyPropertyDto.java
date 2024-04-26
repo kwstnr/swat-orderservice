546095d7f6a52883e88df86a2a9cd5f79bdaa5ba
Kevin Wüstner <kevin.wuestner@xappido.com>
2024-04-26 19:33:52 +0200

    feat(verify-property-dto): add verify-property-dto-builder

--- src/main/java/ch/hslu/swda/g06/order/model/VerifyPropertyDto.java
+++ src/main/java/ch/hslu/swda/g06/order/model/VerifyPropertyDto.java
@@ -6,6 +6,11 @@ public class VerifyPropertyDto<T> {
     private boolean verified;
     private Reason reason;
 
+    public VerifyPropertyDto(final String orderId, final T propertyValue, final boolean verified, final Reason reason) {
+        this(orderId, propertyValue, verified);
+        this.reason = reason;
+    }
+
     public VerifyPropertyDto(final String orderId, final T propertyValue, final boolean verified) {
         this.orderId = orderId;
         this.propertyValue = propertyValue;
@@ -34,5 +39,28 @@ public class VerifyPropertyDto<T> {
 
     public void verify() {
         this.verified = true;
+    }
+
+    public static class Builder {
+        public static <T> OrderId<T> builder() {
+            return orderId -> propertyValue -> verified -> reason -> new VerifyPropertyDto<>(orderId, propertyValue,
+                    verified, reason);
+        }
+
+        public interface OrderId<T> {
+            PropertyValue<T> withOrderId(String orderId);
+        }
+
+        public interface PropertyValue<T> {
+            Verified<T> withPropertyValue(T propertyValue);
+        }
+
+        public interface Verified<T> {
+            ReasonStage<T> withVerified(boolean verified);
+        }
+
+        public interface ReasonStage<T> {
+            VerifyPropertyDto<T> withReason(Reason reason);
+        }
     }
 }
